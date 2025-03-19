3b0c72a0f754621a981088777ac50012b2f27ca4
anxielray <anxielworld@gmail.com>
2025-03-19 01:28:44 -0400

    [Models]: |Methods| {Add some methods that will be applied to the Rubik's cube}

---
+++ models/methods.go
@@ -0,0 +1,91 @@
+package models
+
+
+func (r *Rubik_cube)Reshape_3D(color string) Rubik_cube {
+
+}
+
+func (r *Rubik_cube)U() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)U_p() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)R() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)R_p() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)L() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)L_p() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)F() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)F_p() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)B() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)B_p() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)D() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
+func (r *Rubik_cube)D_p() Rubik_cube {
+
+	tmp_r := *r
+
+	r = &tmp_r
+	return *r
+}
