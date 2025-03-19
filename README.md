f13efd47565e2b6aefad62cb7490d2ddd8ed6dbe
anxielray <anxielworld@gmail.com>
2025-03-19 01:29:28 -0400

    [*]: Add modifications on to the projecgt

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Rubik_cube
 
-- The repository will contain code that will be used to solve the rubik's cube. The solution will be implemented in different languages
+- *The repository will contain code that will be used to solve the rubik's cube. The solution will be implemented in different languages*
+
+## Milestones
+
+### M1
+
+- *BUild the models that define a rubik's cube.*
+- *Add models that define 2/3 types of cubits in the Rubik's cube*
+-
--- models/models.go
+++ models/models.go
@@ -12,3 +12,45 @@ type Corner_cubit struct {
 	Right_face string
 }
 
+type Top_Layer  struct {
+	Left_front Corner_cubit
+	Left_mid Mid_cubit
+	Left_back Corner_cubit
+	Mid_front Mid_cubit
+	Mid_back Mid_cubit
+	Right_front Corner_cubit
+	Right_mid Mid_cubit
+	Right_back Corner_cubit
+	Center_cubit string
+}
+
+type Bottom_Layer  struct {
+	Left_front Corner_cubit
+	Left_mid Mid_cubit
+	Left_back Corner_cubit
+	Mid_front Mid_cubit
+	Mid_back Mid_cubit
+	Right_front Corner_cubit
+	Right_mid Mid_cubit
+	Right_back Corner_cubit
+	Center_cubit string
+}
+
+type Middle_Layer  struct {
+	Left_front Mid_cubit
+	Left_back Mid_cubit
+	Right_front Mid_cubit
+	Right_back Mid_cubit
+	Center_front_cubit string
+	Center_left_cubit string
+	Center_right_cubit string
+	Center_back_cubit string
+}
+
+type Rubik_cube struct {
+	Top_Layer Top_Layer
+	Middle_Layer Middle_Layer
+	Bottom_Layer Bottom_Layer
+	Front_face string
+	Top_face string
+}
