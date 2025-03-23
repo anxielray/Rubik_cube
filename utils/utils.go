148ddd4a87dec2b6ba60c10c7077d6158af06b14
anxielray <anxielworld@gmail.com>
2025-03-23 01:45:01 -0400

    {WIP}[Utils]: Comment out the file contents to clean file for more functions.

--- utils/utils.go
+++ utils/utils.go
@@ -1,38 +1,29 @@
 package utils
 
 
-import (
-//	m "anxiel_cube/models"
-	"fmt"
-	"os"
-	"bufio"
-)
+// import (
+// 	m "anxiel_cube/models"
+// 	"fmt"
+// )
 
 
-//==( This is a function to process the input file content )===
-func Process_input(file_name string) (string, error) {
+// //==( This is a function to process the input file content )===
+// func Process_input(file_name string) string  {	
 
-	f, err := os.Open(file_name)
-	if err != nil {
-		return "", fmt.Errorf("processing file input: {%v}", err)
-	}
-	var combined_line string
+// 	f, err := os.Open(file_name)
+// 	if err != nil {
+// 		return "", fmt.Errorf("processing file input: {%v}", err)
+// 	}
 	
-	new_scanner := bufio.NewScanner(f)
-	for new_scanner.Scan() {
-		line := new_scanner.Text()
-		combined_line += line+"\t"
-	}
-
-	return combined_line, nil
-}
+// 	new_scanner := bufio.NewScanner(file)
+// 	for new_scanner.Scan() {
+// 		line := new_scanner.Text()
+// 	}
+// }
 
 
-//==( This is a function to load the input into the Rubik's cube object
-//func Populate_puzzle(input string) *m.Rubik_cube {
+// //==( This is a function to load the input into the Rubik's cube object
+// func Populate_puzzle(input string) *m.Rubik_cube {
 
-//	var tmp_r  m.Rubik_cube
-
-//	tmp_r
-//	return *tmp_r
-//}
+		
+// }
