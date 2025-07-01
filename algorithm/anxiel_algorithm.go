8f8936fa3497712c50d8b7ba4b555ac9ab20bfe1
anxielray <anxielworld@gmail.com>
2025-07-01 12:29:05 -0400

    fixbugs(controls): Fix the controls  and the mismatch of the eventual cubits

--- algorithm/anxiel_algorithm.go
+++ algorithm/anxiel_algorithm.go
@@ -4,6 +4,7 @@ import (
 	m "anxiel_cube/models"
 )
 
+// solve Rubic cube invokes the algorithm to solve the rubic cube and returns the commands used to solve the problm.
 func SolveRC(puzzle *m.Rubik_cube) []string {
 
 	_ = BottomCross(puzzle)
