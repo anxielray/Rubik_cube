92de806354401f4cedbb1a1f53e81570a0fe4cd9
anxielray <anxielworld@gmail.com>
2025-07-02 12:51:50 -0400

    fixbugs(clean): Optimize and fi mismatched cubits to be correct.

--- models/controls.go
+++ models/controls.go
@@ -25,13 +25,13 @@ func (r *Rubik_cube) U_p() *Rubik_cube {
 	tmp_r := *r
 	tmp_r.Top_Layer = Top_Layer{
 		Left_front:   Corner_cubit{Top_face: r.Top_Layer.Left_back.Top_face, Front_face: r.Top_Layer.Left_back.Side_face, Side_face: r.Top_Layer.Left_back.Front_face},
-		Left_mid:     Mid_cubit{Top_face: r.Top_Layer.Mid_back.Top_face, Front_face: r.Top_Layer.Mid_front.Front_face},
+		Left_mid:     r.Top_Layer.Mid_back,
 		Left_back:    Corner_cubit{Top_face: r.Top_Layer.Right_back.Top_face, Front_face: r.Top_Layer.Right_back.Front_face, Side_face: r.Top_Layer.Right_back.Side_face},
-		Mid_front:    Mid_cubit{Top_face: r.Top_Layer.Left_mid.Top_face, Front_face: r.Top_Layer.Left_mid.Front_face},
-		Mid_back:     Mid_cubit{Top_face: r.Top_Layer.Right_mid.Top_face, Front_face: r.Top_Layer.Right_mid.Front_face},
+		Mid_front:    r.Top_Layer.Left_mid,
+		Mid_back:     r.Top_Layer.Right_mid,
 		Center_cubit: r.Top_Layer.Center_cubit,
 		Right_front:  Corner_cubit{Top_face: r.Top_Layer.Left_front.Top_face, Front_face: r.Top_Layer.Left_front.Side_face, Side_face: r.Top_Layer.Left_front.Front_face},
-		Right_mid:    Mid_cubit{Top_face: r.Top_Layer.Mid_front.Top_face, Front_face: r.Top_Layer.Mid_front.Front_face},
+		Right_mid:    r.Top_Layer.Mid_front,
 		Right_back:   Corner_cubit{Top_face: r.Top_Layer.Right_front.Top_face, Front_face: r.Top_Layer.Right_front.Front_face, Side_face: r.Top_Layer.Right_front.Side_face},
 	}
 	r = &tmp_r
@@ -198,13 +198,13 @@ func (r *Rubik_cube) F() *Rubik_cube {
 	tmp_r := Rubik_cube{
 		Top_Layer: Top_Layer{
 			Left_front:   Corner_cubit{Top_face: r.Bottom_Layer.Left_front.Side_face, Front_face: r.Bottom_Layer.Left_front.Front_face, Side_face: r.Bottom_Layer.Left_front.Top_face},
-			Left_mid:     r.Top_Layer.Left_mid,
-			Left_back:    r.Top_Layer.Left_back,
 			Mid_front:    Mid_cubit{Top_face: r.Middle_Layer.Left_front.Top_face, Front_face: r.Middle_Layer.Left_front.Front_face},
-			Mid_back:     r.Top_Layer.Mid_back,
 			Right_front:  Corner_cubit{Top_face: r.Top_Layer.Left_front.Side_face, Front_face: r.Top_Layer.Left_front.Front_face, Side_face: r.Top_Layer.Left_front.Top_face},
 			Right_mid:    r.Top_Layer.Right_mid,
 			Center_cubit: r.Top_Layer.Center_cubit,
+			Left_mid:     r.Top_Layer.Left_mid,
+			Left_back:    r.Top_Layer.Left_back,
+			Mid_back:     r.Top_Layer.Mid_back,
 			Right_back:   r.Top_Layer.Right_back,
 		},
 		Middle_Layer: Middle_Layer{
@@ -224,8 +224,8 @@ func (r *Rubik_cube) F() *Rubik_cube {
 			Mid_front:    Mid_cubit{Top_face: r.Middle_Layer.Right_front.Top_face, Front_face: r.Middle_Layer.Right_front.Front_face},
 			Mid_back:     r.Bottom_Layer.Mid_back,
 			Right_front:  Corner_cubit{Top_face: r.Top_Layer.Right_front.Side_face, Front_face: r.Top_Layer.Right_front.Front_face, Side_face: r.Top_Layer.Right_front.Top_face},
-			Right_mid:    Mid_cubit{Top_face: r.Middle_Layer.Right_back.Top_face, Front_face: r.Middle_Layer.Right_back.Front_face},
-			Right_back:   Corner_cubit{Top_face: r.Top_Layer.Right_back.Side_face, Front_face: r.Top_Layer.Right_back.Front_face, Side_face: r.Top_Layer.Right_back.Top_face},
+			Right_mid:    r.Bottom_Layer.Right_mid,
+			Right_back:   r.Bottom_Layer.Right_back,
 			Center_cubit: r.Bottom_Layer.Center_cubit,
 		},
 	}
@@ -315,20 +315,20 @@ func (r *Rubik_cube) B_p() *Rubik_cube {
 	tmp_r := Rubik_cube{
 		Top_Layer: Top_Layer{
 			Left_front:   r.Top_Layer.Left_front,
-			Left_mid:     r.Top_Layer.Left_mid,
-			Left_back:    Corner_cubit{Top_face: r.Bottom_Layer.Left_back.Side_face, Front_face: r.Bottom_Layer.Left_back.Front_face, Side_face: r.Bottom_Layer.Left_back.Top_face},
 			Mid_front:    r.Top_Layer.Mid_front,
-			Mid_back:     r.Middle_Layer.Left_back,
-			Center_cubit: r.Top_Layer.Center_cubit,
 			Right_front:  r.Top_Layer.Right_front,
 			Right_mid:    r.Top_Layer.Right_mid,
-			Right_back:   Corner_cubit{Top_face: r.Top_Layer.Left_back.Side_face, Front_face: r.Top_Layer.Left_back.Front_face, Side_face: r.Top_Layer.Left_back.Top_face},
+			Center_cubit: r.Top_Layer.Center_cubit,
+			Left_mid:     r.Top_Layer.Left_mid,
+			Left_back:    Corner_cubit{Top_face: r.Bottom_Layer.Left_back.Side_face, Front_face: r.Bottom_Layer.Left_back.Front_face, Side_face: r.Bottom_Layer.Left_back.Top_face},
+			Mid_back:     r.Middle_Layer.Left_back,
+			Right_back:   Corner_cubit{Top_face: r.Top_Layer.Left_back.Side_face, Front_face: r.Top_Layer.Left_back.Top_face, Side_face: r.Top_Layer.Left_back.Front_face},
 		},
 		Middle_Layer: Middle_Layer{
 			Left_front:         r.Middle_Layer.Left_front,
 			Left_back:          r.Bottom_Layer.Mid_back,
 			Right_front:        r.Middle_Layer.Right_front,
-			Right_back:         r.Top_Layer.Mid_back,
+			Right_back:         Mid_cubit{Top_face: r.Top_Layer.Mid_back.Front_face, Front_face: r.Top_Layer.Mid_back.Top_face},
 			Center_front_cubit: r.Middle_Layer.Center_front_cubit,
 			Center_back_cubit:  r.Middle_Layer.Center_back_cubit,
 			Center_right_cubit: r.Middle_Layer.Center_right_cubit,
@@ -337,9 +337,9 @@ func (r *Rubik_cube) B_p() *Rubik_cube {
 		Bottom_Layer: Bottom_Layer{
 			Left_front:   r.Bottom_Layer.Left_front,
 			Left_mid:     r.Bottom_Layer.Left_mid,
-			Left_back:    Corner_cubit{Top_face: r.Bottom_Layer.Left_back.Side_face, Front_face: r.Bottom_Layer.Left_back.Front_face, Side_face: r.Bottom_Layer.Left_back.Top_face},
+			Left_back:    Corner_cubit{Top_face: r.Bottom_Layer.Right_back.Front_face, Front_face: r.Bottom_Layer.Right_back.Side_face, Side_face: r.Bottom_Layer.Right_back.Top_face},
 			Mid_front:    r.Bottom_Layer.Mid_front,
-			Mid_back:     r.Middle_Layer.Right_back,
+			Mid_back:     Mid_cubit{Top_face: r.Middle_Layer.Right_back.Front_face, Front_face: r.Middle_Layer.Right_back.Top_face},
 			Right_front:  r.Bottom_Layer.Right_front,
 			Right_mid:    r.Bottom_Layer.Right_mid,
 			Right_back:   Corner_cubit{Top_face: r.Top_Layer.Right_back.Front_face, Front_face: r.Top_Layer.Right_back.Top_face, Side_face: r.Top_Layer.Right_back.Side_face},
