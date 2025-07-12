39c932381855c7453bba58b391c465df96d7e315
anxielray <anxielworld@gmail.com>
2025-07-12 10:21:09 -0400

    feature(add): Add the function evocation of the middle layer functionality algorithm

--- algorithm/anxiel.go
+++ algorithm/anxiel.go
@@ -7,7 +7,9 @@ import (
 // solve Rubic cube invokes the algorithm to solve the rubic cube and returns the commands used to solve the problm.
 func SolveRC(puzzle *m.Rubik_cube) []string {
 	c := BottomCross(puzzle)
-	_ = BottomCorners(c)
+	c = BottomCorners(c)
+	c = m.Rotation(c, "y", -90)
+	_ = MidAllign(c)
 
 	return m.Commands
 }
