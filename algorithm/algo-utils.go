5a271568b62709396ebdbe44fb22a7215b248b37
anxielray <anxielworld@gmail.com>
2025-07-12 10:20:31 -0400

    rename(function): Rename the functions used in the bottom layer algorithm to avoid confusion with the middle layer algorithm functions

--- algorithm/algo-utils.go
+++ algorithm/algo-utils.go
@@ -29,7 +29,7 @@ func CheckCross(c *m.Rubik_cube, refColor string) bool {
 // locate corner function takes a cube
 // locates the corner cubit and alligns the cubit correlclty
 // returns a number as detailed in the notes.txt file
-func locateCorner(c *m.Rubik_cube) int {
+func bottomLocateCorner(c *m.Rubik_cube) int {
 	var (
 		p1 = c.Top_Layer.Left_front
 		p2 = c.Top_Layer.Left_back
@@ -41,28 +41,28 @@ func locateCorner(c *m.Rubik_cube) int {
 		p8 = c.Bottom_Layer.Right_back
 	)
 
-	if combo(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p1) {
+	if comboB(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p1) {
 		return 1
-	} else if combo(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p2) {
+	} else if comboB(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p2) {
 		return 2
-	} else if combo(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p3) {
+	} else if comboB(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p3) {
 		return 3
-	} else if combo(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p4) {
+	} else if comboB(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p4) {
 		return 4
-	} else if combo(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p5) {
+	} else if comboB(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p5) {
 		return 5
-	} else if combo(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p6) {
+	} else if comboB(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p6) {
 		return 6
-	} else if combo(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p7) {
+	} else if comboB(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p7) {
 		return 7
-	} else if combo(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p8) {
+	} else if comboB(c.Top_Layer.Mid_front.Top_face, c.Top_Layer.Mid_front.Front_face, c.Middle_Layer.Left_front.Top_face, p8) {
 		return 8
 	}
 	return 0
 }
 
 // correct combo fucntion is a function that confirms if the correct combination of corner colors have been met for the corner
-func combo(ct, cf, cs string, cubit m.Corner_cubit) bool {
+func comboB(ct, cf, cs string, cubit m.Corner_cubit) bool {
 	var (
 		T = cubit.Top_face
 		F = cubit.Front_face
@@ -126,4 +126,57 @@ func sortCorner(c *m.Rubik_cube) *m.Rubik_cube {
 	c = c.F()
 	m.Commands = append(m.Commands, "F")
 	return c
+}
+
+// locate corner function takes a cube
+// locates the corner cubit and alligns the cubit correlclty
+// returns a number as detailed in the notes.txt file
+func midLocateEdge(c *m.Rubik_cube) int {
+	var (
+		p1 = c.Middle_Layer.Left_front
+		p2 = c.Middle_Layer.Right_front
+		p3 = c.Middle_Layer.Right_back
+		p4 = c.Middle_Layer.Left_back
+		p5 = c.Top_Layer.Mid_front
+		p6 = c.Top_Layer.Right_mid
+		p7 = c.Top_Layer.Mid_back
+		p8 = c.Top_Layer.Left_mid
+	)
+
+	if comboM(c.Middle_Layer.Center_left_cubit, c.Middle_Layer.Center_front_cubit, p1) {
+		return 1
+	} else if comboM(c.Middle_Layer.Center_left_cubit, c.Middle_Layer.Center_front_cubit, p2) {
+		return 2
+	} else if comboM(c.Middle_Layer.Center_left_cubit, c.Middle_Layer.Center_front_cubit, p3) {
+		return 3
+	} else if comboM(c.Middle_Layer.Center_left_cubit, c.Middle_Layer.Center_front_cubit, p4) {
+		return 4
+	} else if comboM(c.Middle_Layer.Center_left_cubit, c.Middle_Layer.Center_front_cubit, p5) {
+		return 5
+	} else if comboM(c.Middle_Layer.Center_left_cubit, c.Middle_Layer.Center_front_cubit, p6) {
+		return 6
+	} else if comboM(c.Middle_Layer.Center_left_cubit, c.Middle_Layer.Center_front_cubit, p7) {
+		return 7
+	} else if comboM(c.Middle_Layer.Center_left_cubit, c.Middle_Layer.Center_front_cubit, p8) {
+		return 8
+	}
+	return 0
+}
+
+// function to find a  matching combination of the edge cubits
+func comboM(et, ef string, cubit m.Mid_cubit) bool {
+	var (
+		T = cubit.Top_face
+		F = cubit.Front_face
+	)
+
+	if T == et && F == ef {
+		return true
+	}
+
+	if F == et && T == ef {
+		return true
+	}
+
+	return false
 }
