1858c659e6a8c32ca973caf362a716b16138be3b
anxielray <anxielworld@gmail.com>
2025-07-12 07:14:37 -0400

    feature(add):  Add controls  and algorithm to handle the  case 7, 5 and 8 instances of the puzzle cudbe for the bottom layer

--- algorithm/bottom_corners.go
+++ algorithm/bottom_corners.go
@@ -12,45 +12,65 @@ func BottomCorners(c *m.Rubik_cube) *m.Rubik_cube {
 	c = alignTheCube(c)
 	// locate the target corner cubit
 	for !cornersAllign(c) {
-
 		position := locateCorner(c)
 		fmt.Println(position)
 		switch position {
 		case 1, 2:
 			c = sortCorner(c)
-			position = 0
 		case 3:
 			c = c.U().R_p().U_p().R()
-			m.Commands = append(m.Commands, "B'")
+			m.Commands = append(m.Commands, "U")
+			m.Commands = append(m.Commands, "R'")
+			m.Commands = append(m.Commands, "U'")
+			m.Commands = append(m.Commands, "R")
 			c = sortCorner(c)
-			position = 0
 		case 4:
 			c = c.B()
-			m.Commands = append(m.Commands, "B'")
+			m.Commands = append(m.Commands, "B")
 			c = sortCorner(c)
-			position = 0
-		case 5: // rotate 90, do case 1 without the B' in one rotation, rotate -90 perform case 1 recursively including B'
+		case 5:
+			c = m.Rotation(c, "y", -90)
+			c = m.Rotation(c, "x", 90)
+			c = m.Rotation(c, "y", 90)
+			c = c.L().U_p().L_p().U()
+			c = m.Rotation(c, "y", -90)
+			c = m.Rotation(c, "x", -90)
+			c = m.Rotation(c, "y", 90)
+
+			m.Commands = append(m.Commands, "L")
+			m.Commands = append(m.Commands, "U'")
+			m.Commands = append(m.Commands, "L'")
+			m.Commands = append(m.Commands, "U")
 			c = c.B_p()
 			m.Commands = append(m.Commands, "B'")
 			c = sortCorner(c)
-			position = 0
 		case 6:
 			c = c.B_p()
 			m.Commands = append(m.Commands, "B'")
 			c = sortCorner(c)
-			position = 0
-		case 7: // rotate -180, perform case 1 without B' in one rotation; perform case 8
-			c = c.B_p()
-			m.Commands = append(m.Commands, "B'")
+		case 7:
+			c = m.Rotation(c, "y", 90)
+			c = m.Rotation(c, "x", 90)
+			c = m.Rotation(c, "y", -90)
+			c = c.U().R_p().U_p().R()
+			c = m.Rotation(c, "y", 90)
+			c = m.Rotation(c, "x", -90)
+			c = m.Rotation(c, "y", -90)
+			c = c.B().B()
+			m.Commands = append(m.Commands, "U")
+			m.Commands = append(m.Commands, "R'")
+			m.Commands = append(m.Commands, "U'")
+			m.Commands = append(m.Commands, "R")
+			m.Commands = append(m.Commands, "2B")
 			c = sortCorner(c)
-			position = 0
-		case 8: // rotate  2B then perform case 1 recursively without B'
-
-			c = c.B_p()
-			m.Commands = append(m.Commands, "B'")
+		case 8:
+			c = c.B().B()
+			m.Commands = append(m.Commands, "2B")
 			c = sortCorner(c)
-			position = 0
+		}
+		if cornersAllign(c) {
+			break
 		}
 	}
-	return c
+	return alignTheCube(c)
 }
